fea186e0f1c235a6cd9e4aa09cd355066ad6b0fb
Harsh Shah <harsh.professional02@gmail.com>
2026-09-24 01:24:30 -0400

    Add 0015-3sum

--- 0015-3sum/solution.py
+++ 0015-3sum/solution.py
@@ -1,15 +1,15 @@
 # 15. 3Sum [Medium]
 # https://leetcode.com/problems/3sum/
-# Accepted 2026-09-17  runtime 507 ms  memory 22.2 MB
+# Accepted 2026-09-24  runtime 542 ms  memory 22.1 MB
 
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
         res = []
-        for i, n in enumerate(nums):
+        for i , n in enumerate(nums):
             if i > 0 and n == nums[i-1]:
                 continue
-            l , r = i + 1, len(nums) - 1
+            l, r = i + 1, len(nums) - 1
             while l < r:
                 total = n + nums[l] + nums[r]
                 if total > 0:
@@ -17,8 +17,8 @@ class Solution:
                 elif total < 0:
                     l += 1
                 else:
-                    res.append([n, nums[l], nums[r]])
+                    res.append([n, nums[l],nums[r]])
                     l += 1
-                    while l < r and nums[l]== nums[l-1]:
+                    while l < r and nums[l] == nums[l - 1]:
                         l += 1
         return res
